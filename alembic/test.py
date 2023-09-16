4f51c21d3abae448b361f855a2a47f18c3969865
JD Solanki <jdsolanki0001@gmail.com>
2023-09-16 20:56:38 +0530

    chore: delete temp alembic/test.py file

--- alembic/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import pathlib
-
-from dotenv import load_dotenv
-
-curr_dir = pathlib.Path(__file__).parent.resolve()
-load_dotenv(dotenv_path=curr_dir.parent / ".env")
-
-print(os.getenv("DB_USER"))
